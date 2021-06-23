8591c32999e755e32d69d0155503de20c13d8f64
bigboyjuicer <79086305+bigboyjuicer@users.noreply.github.com>
2021-06-23 11:50:03 +0300

    Add files via upload

--- main.php
+++ main.php
@@ -4,9 +4,11 @@
 <title>Главная страница</title>
 </head>
 <body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <?php 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php
         $db = pg_connect("host='localhost' dbname='php_project' user='postgres' password='123'");
     ?>
     <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
@@ -15,17 +17,26 @@
         <img src="https://img.icons8.com/wired/64/000000/ambulance.png" alt="" width="45" height="45">
         Запись к врачу</a>
         <div class="row justify-content-end">
-        <form name='' action='' method='POST'>
+        <form action='' method='GET'>
                 <div class="row justify-content-around">
                     <font size="+2">
-                    <a href='' class="navbar-brand mb-1 h1">
+                    <a href='profile.php' type="button" class="btn btn-outline-dark">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    </svg>
                     <?php
                         echo($_COOKIE['username']);
                     ?>
                     </a>
+                    <a href='main.php?exit=true'type='submit' name='delete' class="btn btn-outline-dark">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                        <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                    </svg>
+                    </a>
                     </font>
+                    <!-- <input type='submit' name='delete' value='Выйти из аккаунта' class="btn btn-outline-danger btn-sm"> -->
                 </div>
-                <input type='submit' name='delete' value='Выйти из аккаунта' class="btn btn-outline-dark btn-sm">
         </form>
         </div>
     </div>
@@ -40,7 +51,7 @@
         <th scope="col">Действие</th>
         </tr>
     </thead>
-    <?php 
+    <?php
         $query = 'select * from doctor';
         $result = pg_query($db, $query);
         while($row = pg_fetch_assoc($result)){
@@ -63,8 +74,7 @@
             <?php }
             else{?>
             <a href="makerecord.php?id=<?php echo $row['id']; ?>" name='makerecord' class="btn btn-primary"> Записаться </a>
-            <?php } 
-            ?>
+            <?php } ?>
         </form>
         </td>
         </tr>
@@ -72,16 +82,15 @@
     <?php }
     ?>
     </table>
-     
-    <?php 
-        if(isset($_POST['delete'])){
+    <?php
+        if(isset($_GET['exit'])){
             setcookie('username','');
             setcookie('password','');
             header('Location: auth.php');
         }
         if(!isset($_COOKIE['username']) and !isset($_COOKIE['password'])){
-        header('Location: auth.php');
+            header('Location: auth.php');
         }
-        ?>
+    ?>
 </body>
 </html>
